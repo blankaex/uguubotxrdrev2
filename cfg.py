a5d831bea4212eb9d519095922032ef623c1d323
nightmare_blankaex@hotmail.com <nightmare_blankaex@hotmail.com>
2018-10-28 13:08:50 +0000

    Added cfg file

---
+++ cfg.py
@@ -0,0 +1,10 @@
+import re
+
+HOST = "irc.twitch.tv"
+PORT = 6667
+NICK = "UguuBotXrdRev2"
+PASS = # Redacted
+CHAN = "#blankaexx"
+RATE = (20/30)
+CHAT = re.compile(r"^:\w+!\w+@\w+\.tmi\.twitch\.tv PRIVMSG #\w+ :")
+MODS = ["blankaexx"]
