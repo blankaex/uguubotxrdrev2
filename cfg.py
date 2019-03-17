4d91b3da60558fc1f931efe9032e5b562a54abc6
nightmare_blankaex@hotmail.com <nightmare_blankaex@hotmail.com>
2019-03-17 05:35:18 +0000

    Implemented Twitch API v5 GET/SET and Time methods

--- cfg.py
+++ cfg.py
@@ -5,11 +5,15 @@ HOST = "irc.twitch.tv"
 PORT = 6667
 NICK = "UguuBotXrdRev2"
 PASS = # Redacted
+ACPT = "application/vnd.twitchtv.v5+json"
+CLID = # Redacted
+AUTH = # Redacted
+CURL = "https://api.twitch.tv/kraken/channels/41790391"
 CHAN = "#blankaexx"
 RATE = (20/30)
 CHAT = re.compile(r"^:\w+!\w+@\w+\.tmi\.twitch\.tv PRIVMSG #\w+ :")
 MODS = ["blankaexx"]
-CMDL = ["link", "twitter", "youtube", "paizuri"]
+CMDL = ["time", "link", "twitter", "youtube", "paizuri"]
 
 def isCommand(line):
     return line[0] == '!'
@@ -22,4 +26,3 @@ def getMsg(line):
 
 def chat(sock, msg):
     sock.send(("PRIVMSG {} :{}\r\n".format(CHAN, msg)).encode("utf-8"))
-
--- cmd.py
+++ cmd.py
@@ -1,4 +1,7 @@
+import requests
 import datetime
+import pytz
+import shlex
 from cfg import *
 
 commands = {}
@@ -29,30 +32,40 @@ def untimeout(sock, name, user):
     if name in MODS:
         chat(sock, ".untimeout {}".format(user))
 
-@loadCommands("title")
-def title(sock, name, title):
+@loadCommands("get")
+def get(sock, name, index):
     if name in MODS:
-        # TODO
+        arg = "status" if index == "title" else index
+        h = {'Accept': ACPT, 'Client-ID': CLID}
+        r = requests.get(CURL, headers=h)
+        if r.status_code == requests.codes.ok:
+            chat(sock, dict(r.json())[arg])
+        else:
+            chat(sock, "Invalid command")
         return
 
-@loadCommands("game", "category")
-def game(sock, name, game):
+@loadCommands("set")
+def game(sock, name, index, value):
     if name in MODS:
-        # TODO
+        arg = "status" if index == "title" else index
+        h = {'Accept': ACPT, 'Client-ID': CLID, 'Authorization': AUTH}
+        d = {'channel[{}]'.format(arg): value}
+        r = requests.put(CURL, headers=h, data=d)
+        if r.status_code == requests.codes.ok:
+            chat(sock, "{} set to: {}".format(index[0].upper() + index[1:], value))
+        else:
+            chat(sock, "Invalid command")
         return
 
-@loadCommands("help", "list", "commands")
+@loadCommands("help", "commands", "h")
 def help(sock, name):
     chat(sock, "Commands: !" + ", !".join(CMDL))
 
 @loadCommands("time", "t", "current time", "currtime", "ct")
 def currtime(sock, name):
-    print(datetime.now(tz=timezone('Australia/Sydney')))
-
-@loadCommands("uptime", "ut")
-def uptime(sock, name):
-    # TODO
-    return
+    tz = pytz.timezone('Australia/Sydney')
+    now = datetime.datetime.now(tz).strftime("%H:%M, %d %b")
+    chat(sock, "Current time in Sydney is {}.".format(now))
 
 @loadCommands("link", "stream", "streamlink", "twitch")
 def link(sock, name):
@@ -68,16 +81,17 @@ def youtube(sock, name):
 
 @loadCommands("paizuri")
 def paizuri(sock, name): 
-    chat(sock, "Th-that's lewd, {}".format(name))
-    chat(sock, ".timeout {} 300".format(name))
+    chat(sock, "Th-That's lewd, {} (〃▽〃)".format(name))
+    if name not in MODS:
+        chat(sock, ".timeout {} 300".format(name))
 
 def handle(sock, line):
     name = getName(line)
     msg = getMsg(line)
     print("{}: {}".format(name, msg))
     if isCommand(msg):
-        cmd, *args = msg[1:].lower().split()
+        cmd, *args = shlex.split(msg[1:])
         try:
-            commands[cmd](sock, name, *args)
+            commands[cmd.lower()](sock, name, *args)
         except:
             pass
